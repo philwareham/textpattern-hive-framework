f1749870fb938ea26798c55fec26e2305ebe7125
philwareham <phil@designhive.com>
2020-09-17 10:52:38 +0100

    Some build scaffolding

---
+++ Gruntfile.js
@@ -0,0 +1,191 @@
+module.exports = function (grunt)
+{
+    'use strict';
+
+    // Load all Grunt tasks.
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Set up paths.
+        paths: {
+            src: {
+                sass: 'scss/',
+                fonts: 'node_modules/hive-framework/src/assets/fonts/',
+                images: 'img/',
+                js: 'node_modules/hive-framework/src/assets/js/'
+            },
+            dest: {
+                css: 'themes/hive_framework/assets/css/',
+                fonts: 'themes/hive_framework/assets/fonts/',
+                images: 'themes/hive_framework/assets/img/',
+                js: 'themes/hive_framework/assets/js/'
+            }
+        },
+
+        // Bundle up the JavaScript.
+        browserify: {
+            development: {
+                src: [
+                    '<%= paths.src.js %>app.js'
+                ],
+                dest: '<%= paths.dest.js %>app.js',
+                options: {
+                    browserifyOptions: {
+                        debug: false
+                    },
+                    transform: [[
+                        'babelify', {
+                            'presets': ['@babel/preset-env']
+                        }
+                    ]]
+                }
+            }
+        },
+
+        // Clean theme directory to start afresh.
+        clean: [
+            'themes/*'
+        ],
+
+        // Run some tasks in parallel to speed up the build process.
+        concurrent: {
+            dist: [
+                'browserify',
+                'copy:fonts',
+                'copy:img',
+                'css',
+                'jshint'
+            ]
+        },
+
+        copy: {
+            // Copy fonts.
+            fonts: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= paths.src.fonts %>',
+                        src: '**',
+                        dest: '<%= paths.dest.fonts %>'
+                    }
+                ]
+            },
+            // Copy fonts.
+            img: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= paths.src.images %>',
+                        src: '**',
+                        dest: '<%= paths.dest.images %>'
+                    }
+                ]
+            }
+        },
+
+        // Check code quality of Gruntfile.js and site-specific JavaScript using JSHint.
+        jshint: {
+            options: {
+                bitwise: true,
+                browser: true,
+                curly: true,
+                eqeqeq: true,
+                esversion: 6,
+                forin: true,
+                globals: {
+                    $: true,
+                    console: true, // Comment this out for production code.
+                    jQuery: true,
+                    Zepto: true,
+                    define: true,
+                    module: true,
+                    require: true,
+                    autosize: true,
+                    Prism: true
+                },
+                latedef: true,
+                noarg: true,
+                nonew: true,
+                strict: true,
+                undef: true,
+                unused: true
+            },
+            files: [
+                'Gruntfile.js',
+                '<%= paths.src.js %>*.js'
+            ]
+        },
+
+        // Add vendor prefixed styles and other post-processing transformations.
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer'),
+                    require('cssnano')
+                ]
+            },
+            dist: {
+                src: '<%= paths.dest.css %>*.css'
+            }
+        },
+
+        // Sass configuration.
+        sass: {
+            options: {
+                implementation: require('sass'),
+                outputStyle: 'expanded', // outputStyle = expanded, nested, compact or compressed.
+                sourceMap: false
+            },
+            dist: {
+                files: [
+                    {'<%= paths.dest.css %>style.css': '<%= paths.src.sass %>style.scss'},
+                    {'<%= paths.dest.css %>design-patterns.css': '<%= paths.src.sass %>design-patterns.scss'}
+                ]
+            }
+        },
+
+        // Validate CSS files via stylelint.
+        stylelint: {
+            options: {
+                configFile: '.stylelintrc.yml'
+            },
+            src: ['<%= paths.src.sass %>**/*.{css,scss}']
+        },
+
+        // Minify `app.js`.
+        uglify: {
+            dist: {
+                files: [
+                    {
+                        '<%= paths.dest.js %>app.js': ['<%= paths.dest.js %>app.js']
+                    }
+                ]
+            }
+        },
+
+        // Directories watched and tasks performed by invoking `grunt watch`.
+        watch: {
+            sass: {
+                files: '<%= paths.src.sass %>**/*.scss',
+                tasks: 'css'
+            },
+            js: {
+                files: '<%= paths.src.js %>**',
+                tasks: [
+                    'jshint',
+                    'browserify',
+                    'uglify'
+                ]
+            }
+        }
+
+    });
+
+    // Register tasks.
+    grunt.registerTask('build', ['clean', 'concurrent', 'uglify']);
+    grunt.registerTask('css', ['stylelint', 'sass', 'postcss']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('travis', ['build']);
+};
